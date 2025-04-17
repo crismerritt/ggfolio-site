9cbec5b7a503d5f0853e4349ba854ad631fb3c27
Cris Merritt <crismerritt@gmail.com>
2025-04-17 15:11:45 -0400

    placeholder in main

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,16 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    { /* Uncomment the line below to render the App component */ }
+    { /* <App /> */ }
+
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <img 
+        src="/ggfolio_logo_dark_bg.png" // Replace with your image path
+        alt="ggfolio logo" 
+        style={{ maxWidth: '100%', height: 'auto' }} 
+      />
+    </div>
+
   </StrictMode>,
 )
